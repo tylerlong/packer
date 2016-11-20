4277eedb3092857241c2cd67d512c4fb18835ba2
Tyler Long <tyler4long@gmail.com>
2016-11-20 18:23:49 +0800

    Pack highlight.js

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ require('font-awesome/css/font-awesome.css');
 require('ionicons/dist/css/ionicons.css');
 require('katex/dist/katex.min.css');
 require('github-markdown-css/github-markdown.css');
+require('highlight.js/styles/atom-one-light.css');
 require('./index.css');
 
+
 const katex = require('katex');
+const hljs = require('highlight.js');
 
 
 const f = () => {
-  katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementById('katex-container'));
+    katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementById('katex-container'));
+    hljs.initHighlightingOnLoad();
 };
 
 f();
