cf5e9f113beb283212313ecc76432de426554dbc
Tyler Long <tyler4long@gmail.com>
2016-11-20 18:10:10 +0800

    Pack github markdown css

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('font-awesome/css/font-awesome.css');
 require('ionicons/dist/css/ionicons.css');
 require('katex/dist/katex.min.css');
+require('github-markdown-css/github-markdown.css');
+require('./index.css');
+
 const katex = require('katex');
+
 
 const f = () => {
   katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementById('katex-container'));
