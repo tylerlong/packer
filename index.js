93ce4f83bfea5260ddaa82c405e3304e20e739d7
Tyler Long <tyler4long@gmail.com>
2016-11-20 16:32:02 +0800

    pack katex

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('font-awesome/css/font-awesome.css');
 require('ionicons/dist/css/ionicons.css');
+require('katex/dist/katex.min.css');
+const katex = require('katex');
 
 const f = () => {
-  console.log('hello test');
+  katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementById('katex-container'));
 };
 
 f();
