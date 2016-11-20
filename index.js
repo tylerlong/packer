d3dcee0161faef6e4b7faac6da78c5c485746259
Tyler Long <tyler4long@gmail.com>
2016-11-20 11:48:44 +0800

    Successfully pack font-awesome

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log('hello world');
+// require('font-awesome/css/font-awesome.css');
+require("font-awesome-webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   entry: "./index.js",
   output: {
-    path: __dirname,
-    filename: "index.bundle.js",
+    path: __dirname + '/dist',
+    filename: "index.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+    ],
   },
 };
